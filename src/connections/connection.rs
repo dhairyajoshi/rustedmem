0e44887985285f577eb625c80579086660715621
Dhairya <joshidhairaya2002@gmail.com>
2025-11-20 15:59:45 +0530

    fix: using bufReader

--- src/connections/connection.rs
+++ src/connections/connection.rs
@@ -1,19 +1,16 @@
-use std::{
-    io::{Read, Write},
-    net::TcpStream,
-};
-
 use crate::cmd::handler::handle_command;
+use std::io::{BufRead, BufReader};
+use std::{io::Write, net::TcpStream};
 
 pub fn handle_connection(stream: &mut TcpStream) {
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
     loop {
-        let mut buf = [0u8; 512];
-        let bytes_read = stream.read(&mut buf).unwrap();
-        if bytes_read == 0 {
+        let mut command = String::new();
+        let bytes = reader.read_line(&mut command).unwrap();
+        if bytes == 0 {
             break;
         }
-        let command = String::from_utf8_lossy(&buf[..bytes_read]).to_string();
-        let response = handle_command(&command) + "\n";
+        let response = handle_command(&command.trim().to_string()) + "\n";
         stream.write_all(response.as_bytes()).unwrap();
     }
 }
